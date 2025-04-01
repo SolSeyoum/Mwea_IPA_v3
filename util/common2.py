0b0652012877ea6f03a713afc6c8cb534436d71d
Solomon <sd.seyoum@yahoo.com>
2025-04-01 17:46:43 +0200

    Update common2.py

--- util/common2.py
+++ util/common2.py
@@ -109,6 +109,13 @@ def set_page_container_style(
             }
             </style>""", unsafe_allow_html=True)
 
+        hide_github_icon = """
+            <style>
+                .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+            </style>
+        """
+        st.markdown(hide_github_icon, unsafe_allow_html=True)
+
         #######################
         # # CSS styling
         # st.markdown("""
